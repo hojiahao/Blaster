cb6d82e1210e367c843ad387567bdc4d325eef49
hojiahao <hojiahao@outlook.com>
2026-01-03 23:00:52 +0800

    Shrink Crosshairs when Aiming

--- Source/Blaster/BlasterComponents/CombatComponent.cpp
+++ Source/Blaster/BlasterComponents/CombatComponent.cpp
@@ -118,7 +118,31 @@ void UCombatComponent::SetHUDCrosshairs(float DeltaTime)
 					30.f
 				);
 			}
-			HUDPackage.CrosshairSpread = CrosshairVelocityFactor + CrosshairInAirFactor;
+			if (bAiming)
+			{
+				CrosshairAimFactor = FMath::FInterpTo(
+					CrosshairAimFactor,
+					0.58f,
+					DeltaTime,
+					30.f
+				);
+			}
+			else
+			{
+				CrosshairAimFactor = FMath::FInterpTo(
+					CrosshairAimFactor,
+					0.f,
+					DeltaTime,
+					30.f
+				);
+			}
+			CrosshairShootingFactor = FMath::FInterpTo(
+				CrosshairShootingFactor,
+				0.f,
+				DeltaTime,
+				40.f
+			);
+			HUDPackage.CrosshairSpread = 0.5f + CrosshairVelocityFactor + CrosshairInAirFactor - CrosshairAimFactor + CrosshairShootingFactor;
 			HUD->SetHUDPackage(HUDPackage);
 		}
 	}
@@ -184,6 +208,10 @@ void UCombatComponent::FireButtonPressed(bool bPressed)
 		FHitResult HitResult;
 		TraceUnderCrosshairs(HitResult);
 		ServerFire(HitResult.ImpactPoint);
+		if (EquippedWeapon)
+		{
+			CrosshairShootingFactor = 0.75f;
+		}
 	}
 }
 
