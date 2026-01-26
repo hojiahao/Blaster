bb78eed62185cb273428f9e9862a00e5e08322d3
hojiahao <hojiahao@outlook.com>
2026-01-26 17:00:15 +0800

    Adding Spawn Points to Level

--- Source/Blaster/Pickups/PickupSpawnPoint.cpp
+++ Source/Blaster/Pickups/PickupSpawnPoint.cpp
@@ -8,7 +8,7 @@
 APickupSpawnPoint::APickupSpawnPoint()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 	bReplicates = true;
 }
 
