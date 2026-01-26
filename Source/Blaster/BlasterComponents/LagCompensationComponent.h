b41bdc4dceb784ddbb37f0e7c2fe3ca8934941c1
hojiahao <hojiahao@outlook.com>
2026-01-26 22:33:29 +0800

    Interp Between Frames

--- Source/Blaster/BlasterComponents/LagCompensationComponent.h
+++ Source/Blaster/BlasterComponents/LagCompensationComponent.h
@@ -54,6 +54,7 @@ protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
 	void SaveFramePackage(FFramePackage& Package);
+	FFramePackage InterpBetweenFrames(const FFramePackage& OlderFrame, const FFramePackage& YoungerFrame, float HitTime);
 private:
 	UPROPERTY()
 	TObjectPtr<ABlasterCharacter> Character;
