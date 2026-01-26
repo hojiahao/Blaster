f24d9100d5b2d84cfcad7a4211dfeac72cfcbba0
hojiahao <hojiahao@outlook.com>
2026-01-26 22:11:04 +0800

    Hit Boxes

--- Source/Blaster/BlasterComponents/LagCompensationComponent.cpp
+++ Source/Blaster/BlasterComponents/LagCompensationComponent.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "BlasterComponents/LagCompensationComponent.h"
+#include "LagCompensationComponent.h"
 
 // Sets default values for this component's properties
 ULagCompensationComponent::ULagCompensationComponent()
