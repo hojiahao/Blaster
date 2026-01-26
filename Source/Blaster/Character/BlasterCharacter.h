bdcf2c4ab41e4ffae0387eb75310dcc628abf5f9
hojiahao <hojiahao@outlook.com>
2026-01-26 22:25:51 +0800

    Saving a Frame Package

--- Source/Blaster/Character/BlasterCharacter.h
+++ Source/Blaster/Character/BlasterCharacter.h
@@ -52,6 +52,8 @@ public:
 
 	void SpawDefaultWeapon();
 
+	UPROPERTY()
+	TMap<FName, class UBoxComponent*> HitCollisionBoxes;
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
