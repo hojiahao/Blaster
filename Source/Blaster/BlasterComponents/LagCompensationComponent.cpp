9026c036444ebeedfdef316fa4bc86af62f9e5a6
hojiahao <hojiahao@outlook.com>
2026-01-27 11:19:09 +0800

    Requesting a Shotgun Hit & Predict Projectile Path

--- Source/Blaster/BlasterComponents/LagCompensationComponent.cpp
+++ Source/Blaster/BlasterComponents/LagCompensationComponent.cpp
@@ -364,7 +364,7 @@ FFramePackage ULagCompensationComponent::GetFrameToCheck(ABlasterCharacter * Hit
 		// Interpolate between Younger and Older
 		FrameToCheck = InterpBetweenFrames(Older->GetValue(), Younger->GetValue(), HitTime);
 	}
-
+	FrameToCheck.Character = HitCharacter;
 	return FrameToCheck;
 }
 
